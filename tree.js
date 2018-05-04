c445c5b5d683f8e292e1ee089d049589777131d0
jurasm <j.mikelionis@gmail.com>
2018-05-04 13:29:38 +0300

    recursive rendering added

--- tree.js
+++ tree.js
@@ -1,5 +1,7 @@
+// @ts-check
 const recursiveContainer = document.getElementById('recursive');
 const iterativeContainer = document.getElementById('iterative');
+let tree;
 
 async function fetchTree() {
   try {
@@ -20,11 +22,59 @@ function clearContainer(container) {
   }
 }
 
-function render(tree) {
+function renderItem(container, item) {
+  const element = document.createElement('li');
+  element.appendChild(document.createTextNode(item.title));
+
+  const add = document.createElement('a');
+  add.appendChild(document.createTextNode(' [+] '))
+  element.appendChild(add);
+
+  add.addEventListener('click', () => {
+    const title = prompt('Enter title:');
+    item.children.push({
+      title: title || 'Empty title',
+      children: []
+    });
+    render();
+  });
+  
+  const ul = document.createElement('ul')
+  element.appendChild(ul);
+
+  container.querySelector('ul').appendChild(element);
+  return element;
+}
+
+function renderRecursive(tree, container) {
+  if (!container) {
+    recursiveContainer.appendChild(document.createElement('ul'));
+    container = recursiveContainer;
+  }
+
+  tree.forEach(branch => {
+    const item = renderItem(container, branch);
+    if (branch.children.length > 0) {
+      renderRecursive(branch.children, item);
+    }
+  });
+}
+
+function renderIterative(tree) {
+
+}
+
+function render() {
   clearContainer(recursiveContainer);
   clearContainer(iterativeContainer);
+
+  renderRecursive(tree);
+  renderIterative(tree);
 }
 
 document.addEventListener('DOMContentLoaded', function() { 
-  fetchTree().then(render).catch(console.error);
+  fetchTree().then(response => {
+    tree = response;
+    render();
+  }).catch(console.error);
 });
