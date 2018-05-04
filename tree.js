5237850107c1e593857b6a6953a54a05f8062c88
jurasm <j.mikelionis@gmail.com>
2018-05-04 12:53:19 +0300

    fetch tree, then render

--- tree.js
+++ tree.js
@@ -1,5 +1,19 @@
-function render() {
-  console.log('render called');
+const recursiveContainer = document.getElementById('recursive');
+const iterativeConteainer = document.getElementById('iterative');
+
+async function fetchTree() {
+  try {
+    const response = await fetch('tree.json');
+    return response.json();
+  } catch(e) {
+    throw new Error('Fetch failed');
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function() { render(); });
+function render(tree) {
+  console.log(tree);
+}
+
+document.addEventListener('DOMContentLoaded', function() { 
+  fetchTree().then(render).catch(console.error);
+});
